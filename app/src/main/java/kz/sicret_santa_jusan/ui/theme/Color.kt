7cf6affa586123e3c8b8b0823a51dd4fad512d4b
frtank1 <frtank1@yande.ru>
2024-04-04 21:18:11 +0600

    first commit. added core files

---
+++ app/src/main/java/kz/sicret_santa_jusan/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package kz.sicret_santa_jusan.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val BrightOrange = Color(0xFFFF6300)
+val DeepTeal = Color(0xFF01787E)
+val LightBlue = Color(0xFF5AB9BF)
+val PaleBlue = Color(0xFFC0E3E5)
+val Red = Color(0xFFFF0000)
+val DarkGray = Color(0xFF333333)
+val White = Color(0xFFFFFFFF)
